b06cb4e2bdc0f5930618cc410eb6641dcdbeb4a4
zzz1won <92144729+zzz1won@users.noreply.github.com>
2022-01-18 16:38:53 +0900

    인텔리제이를 이용한 스프링부트 과제!

---
+++ src/main/java/com/z1won/memberboard/MemberBoardApplication.java
@@ -0,0 +1,13 @@
+package com.z1won.memberboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/z1won/memberboard/MemberBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.z1won.memberboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
